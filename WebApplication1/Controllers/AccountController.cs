1c3c3eb30e75d00f779196756a314df061a6c378
Kajias1 <sk12376@auca.kg>
2026-09-22 17:38:09 +0600

    Added descriptions to AccountController.cs endpoints using xml docs

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -14,15 +14,35 @@ public class AccountController(IAccountService account) : ControllerBase
 {
     private int UserId => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
 
+    /// <summary>
+    /// Displays balance of the current user.
+    /// </summary>
+    /// <response code="200">Returns the current balance.</response>
     [HttpGet("balance")]
     public async Task<ActionResult<BalanceDto>> GetBalance()
         => new BalanceDto(await account.GetBalanceAsync(UserId));
 
+    /// <summary>
+    /// Deposits money into the current user's account.
+    /// </summary>
+    /// <param name="dto">The amount to deposit.</param>
+    /// <response code="200">Returns the updated balance.</response>
+    /// <response code="400">The amount is invalid.</response>
     [HttpPost("deposit")]
+    [ProducesResponseType(typeof(BalanceDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     public Task<ActionResult<BalanceDto>> Deposit(AmountDto dto)
         => ChangeBalance(dto.Amount, OperationType.Deposit);
 
+    /// <summary>
+    /// Withdraws money from the current user's account.
+    /// </summary>
+    /// <param name="dto">The amount to withdraw.</param>
+    /// <response code="200">Returns the updated balance.</response>
+    /// <response code="400">The amount is invalid.</response>
     [HttpPost("withdraw")]
+    [ProducesResponseType(typeof(BalanceDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     public Task<ActionResult<BalanceDto>> Withdraw(AmountDto dto)
         => ChangeBalance(dto.Amount, OperationType.Withdraw);
 
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
