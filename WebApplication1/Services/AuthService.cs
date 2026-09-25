38338ebd8057fd9d08377cc0ceda9938bd093ca5
Kajias1 <sk12376@auca.kg>
2026-09-25 16:15:31 +0600

    Added protection from timing attacks in AuthService.cs

--- WebApplication1/Services/AuthService.cs
+++ WebApplication1/Services/AuthService.cs
@@ -8,6 +8,9 @@ namespace WebApplication1.Services;
 
 public class AuthService(AppDbContext db, IPasswordHasher<User> hasher) : IAuthService
 {
+    private const string DummyHash =
+        "AQAAAAIAAYagAAAAELZzz6N0GmyTbenCN+JnF/uJasC0i0+vrpb8HWoM33EzPwOIR8+f68F257+q8qbguQ==";
+    
     public async Task<User> RegisterAsync(CredentialsDto dto)
     {
         var name = dto.Name.Trim();
@@ -27,9 +30,11 @@ public class AuthService(AppDbContext db, IPasswordHasher<User> hasher) : IAuthS
         var name = dto.Name.Trim();
         var user = await db.Users.FirstOrDefaultAsync(u => u.Name == name);
 
-        if (user is null ||
-            hasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password)
-            == PasswordVerificationResult.Failed)
+        // Dummy user for constant verification time for protecting from timing attacks
+        var target = user ?? new User { PasswordHash = DummyHash };
+        var result = hasher.VerifyHashedPassword(target, target.PasswordHash!, dto.Password);
+
+        if (user is null || result == PasswordVerificationResult.Failed)
             throw new InvalidCredentialsException();
 
         return user;
